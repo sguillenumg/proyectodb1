f12cc5b4e90cfaba7d54a25ad11641416685b2c4
sguillenumg <sguillen@grupotecun.com>
2021-10-18 17:18:45 -0600

    se agrega logica a recursos de clientes y proveedores

--- apps/entidad/urls.py
+++ apps/entidad/urls.py
@@ -1,5 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from apps.entidad.views_entidad import EntidadViewSet
+from apps.entidad.views_entidad import ClienteViewSet, ProveedorViewSet
+
 router = DefaultRouter(trailing_slash=False)
-router.register(r'entidades/?', EntidadViewSet)
+router.register(r'clientes/?', ClienteViewSet)
+router.register(r'proveedores/?', ProveedorViewSet)
+
 url_entidades = router.urls
--- apps/entidad/views_entidad.py
+++ apps/entidad/views_entidad.py
@@ -1,10 +1,80 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from django.db import transaction
-from apps.entidad.serializer import EntidadSerializer
-from apps.entidad.models import Entidad
+from apps.entidad.serializer import EntidadSerializer, ClienteSerializer, ProveedorSerializer
+from apps.entidad.models import Entidad, Cliente, Proveedor
 
-class EntidadViewSet(viewsets.ModelViewSet):
-    serializer_class = EntidadSerializer
-    queryset = Entidad.objects.all()
-    modelo = Entidad
+class ClienteViewSet(viewsets.ModelViewSet):
+    serializer_class = ClienteSerializer
+    queryset = Cliente.objects.all()
+    modelo = Cliente
+
+    def create(self, request, *args, **kwargs):
+        entidad = EntidadSerializer.json_to_obj(request.data['entidad'])
+
+        cliente = ClienteSerializer.json_to_obj(request.data)
+
+        entidad.save()
+        cliente.save()
+
+        entidad_ser = EntidadSerializer.obj_to_json(entidad)
+        cliente_ser = ClienteSerializer.obj_to_json(cliente)
+
+        cliente_ser['entidad'] = entidad_ser
+
+        return Response(cliente_ser, status=200)
+
+    def update(self, request, *args, **kwargs):
+    
+        entidad = EntidadSerializer.json_to_obj(request.data['entidad'])
+
+        cliente = ClienteSerializer.json_to_obj(request.data)
+
+        entidad.save()
+        cliente.save()
+
+        entidad_ser = EntidadSerializer.obj_to_json(entidad)
+        cliente_ser = ClienteSerializer.obj_to_json(cliente)
+
+        cliente_ser['entidad'] = entidad_ser
+
+        return Response(cliente_ser, status=200)
+
+
+
+class ProveedorViewSet(viewsets.ModelViewSet):
+    serializer_class = ProveedorSerializer
+    queryset = Proveedor.objects.all()
+    modelo = Proveedor
+
+    def create(self, request, *args, **kwargs):
+        entidad = EntidadSerializer.json_to_obj(request.data['entidad'])
+
+        proveedor = ProveedorSerializer.json_to_obj(request.data)
+
+        entidad.save()
+        proveedor.save()
+
+        entidad_ser = EntidadSerializer.obj_to_json(entidad)
+        proveedor_ser = ProveedorSerializer.obj_to_json(proveedor)
+
+        proveedor_ser['entidad'] = entidad_ser
+
+        return Response(proveedor_ser, status=200)
+
+    def update(self, request, *args, **kwargs):
+
+        entidad = EntidadSerializer.json_to_obj(request.data['entidad'])
+
+        proveedor = ProveedorSerializer.json_to_obj(request.data)
+
+        entidad.save()
+        proveedor.save()
+
+        entidad_ser = EntidadSerializer.obj_to_json(entidad)
+        proveedor_ser = ProveedorSerializer.obj_to_json(proveedor)
+
+        proveedor_ser['entidad'] = entidad_ser
+
+        return Response(proveedor_ser, status=200)
